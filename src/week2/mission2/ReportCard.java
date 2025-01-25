8a80244426a38f5a39e258b1c733b476e637e333
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-01-25 15:35:13 +0900

    feat: 학생에 대한 정보를 담는 클래스 생성

--- src/week2/mission2/ReportCard.java
+++ src/week2/mission2/ReportCard.java
@@ -4,6 +4,28 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+class Student {
+    private String name;
+    private int studentID;
+    private String major;
+    private int koreanGrade;
+    private int mathGrade;
+    private int englishGrade;
+    int totalScore;
+    double average;
+
+    public Student(String name, int studentID, String major, int koreanGrade, int mathGrade, int englishGrade) {
+        this.name = name;
+        this.studentID = studentID;
+        this.major = major;
+        this.koreanGrade = koreanGrade;
+        this.mathGrade = mathGrade;
+        this.englishGrade = englishGrade;
+        totalScore = koreanGrade + mathGrade + englishGrade;
+        average = totalScore / 3.0;
+    }
+}
+
 public class ReportCard {
     public static void main(String[] args) {
         String path = "C:\\intelij\\CodeSquad-PreCourse\\input.txt";
