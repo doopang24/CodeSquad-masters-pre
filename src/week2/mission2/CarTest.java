fe9bcc9103a8856750c035ddba3a45734fac7220
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-01-25 12:39:47 +0900

    feat: Car 인터페이스를 구현한 HyundaiCar 클래스 구현

--- src/week2/mission2/CarTest.java
+++ src/week2/mission2/CarTest.java
@@ -3,5 +3,19 @@ package week2.mission2;
 interface Car {
     int showCarNumber();
 }
+class HyundaiCar implements Car {
+    private static int carCount = 0;    // 생성된 자동차 수를 추적
+    private int carNumber;   // 자동차 고유 번호
+
+    public HyundaiCar() {
+        carCount++;
+        this.carNumber = carCount + 1000;
+    }
+
+    @Override
+    public int showCarNumber() {
+        return carNumber;
+    }
+}
 public class CarTest {
 }
