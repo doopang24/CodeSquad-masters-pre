68f9fe47821c5ee660ceec4dbaf6fe00f740dcf4
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-01-25 15:15:48 +0900

    feat: 텍스트 파일을 한 줄씩 읽어오는 기능 추가

--- src/week2/mission2/ReportCard.java
+++ src/week2/mission2/ReportCard.java
@@ -1,4 +1,17 @@
 package week2.mission2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ReportCard {
+    public static void main(String[] args) {
+        String path = "C:\\intelij\\CodeSquad-PreCourse\\input.txt";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String line;
+        } catch (IOException e) {
+            System.out.println("파일을 읽는 중 오류가 발생했습니다. " + e.getMessage());
+        }
+    }
 }
