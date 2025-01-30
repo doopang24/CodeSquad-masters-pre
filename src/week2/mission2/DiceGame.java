159cc3603ef28ab0d5e249bdeb5b51f3e054f846
kaydeen <153696777+doopang24@users.noreply.github.com>
2025-01-30 16:31:27 +0900

    feat: 주사위게임의 결과를 출력하는 메서드 생성

--- src/week2/mission2/DiceGame.java
+++ src/week2/mission2/DiceGame.java
@@ -1,8 +1,20 @@
 package week2.mission2;
 
+import java.util.Random;
+
 class Gamer {
     int money;
     String fruit;
 }
+
 public class DiceGame {
+
+    public int dice() {
+        Random random = new Random();
+        return random.nextInt(7) - 1;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
